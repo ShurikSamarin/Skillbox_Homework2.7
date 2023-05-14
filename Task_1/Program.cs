83297e5d77771b2174a297e0c136920a9425fc73
Alexander <sttp2002@gmail.com>
2023-05-14 12:34:43 +0300

    added formatted output

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -6,10 +6,12 @@ string email = "sttp2002@gmail.com";
 double progBall = 71.5;
 double mathBall = 55.3;
 double physBall = 46.8;
-Console.WriteLine("FIO: "+fullName);
-Console.WriteLine("Age: "+age);
-Console.WriteLine("Email: "+email);
-Console.WriteLine("Programming scores: "+progBall);
-Console.WriteLine("Math scores: "+mathBall);
-Console.WriteLine("Physics scores: "+physBall);
+//Console.WriteLine("FIO: "+fullName);
+//Console.WriteLine("Age: "+age);
+//Console.WriteLine("Email: "+email);
+//Console.WriteLine("Programming scores: "+progBall);
+//Console.WriteLine("Math scores: "+mathBall);
+//Console.WriteLine("Physics scores: "+physBall);
+Console.WriteLine("FIO: {0} | Age: {1} | Email: {2}", fullName, age.ToString(), email); 
+Console.WriteLine("Programming scores: {0} | Math scores: {1} | Physics scores: {2}", progBall.ToString(), mathBall.ToString(), physBall.ToString());
 
